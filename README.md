d9d81a013ef9e4704bd04e8f9433e035c81c9ab5
sushantrajbhar <sushraj44@gmail.com>
2024-10-02 23:44:33 +0800

    requirement.txt added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Object-Detection-end-to-end
+
+##Running the project:
+
+'''bash
+virtualenv object_detection_venv 
+'''
+
+'''bash
+cd .\object_detection_venv\    
+Scripts\activate
+'''
+
+'''bash
+pip install -r requirements.txt
+'''
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Object_Detection',
+    version= '0.0.0',
+    author= 'Sushant',
+    author_email= 'sushraj44@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
